efaf5b24be4cace7d0e3a6407dadc41030c10a15
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2024-09-13 14:35:31 +0200

    Initial push

---
+++ data-raw/icesdata.R
@@ -0,0 +1,11 @@
+# icesdata.R - DESC
+# icesdata/data-raw/icesdata.R
+
+# Copyright (c) WUR, 2024.
+# Author: Iago MOSQUEIRA (WMR) <iago.mosqueira@wur.nl>
+#
+# Distributed under the terms of the EUPL-1.2
+
+# LOAD raw dataset and polish
+
+# SAVE to ../data
