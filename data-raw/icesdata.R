dd59483442a6ecbfc9b357dcf2d073215e61f9ce
lauriekell <laurie@kell.es>
2026-02-16 15:44:20 +0000

    raw

--- data-raw/icesdata.R
+++ data-raw/icesdata.R
@@ -1,11 +1,27 @@
-# icesdata.R - DESC
-# icesdata/data-raw/icesdata.R
+load("C:/active/flr/icesdata/data/raw/Updated_stks_n82_R0_updated202602_15.Rdata")
 
-# Copyright (c) WUR, 2024.
-# Author: Iago MOSQUEIRA (WMR) <iago.mosqueira@wur.nl>
-#
-# Distributed under the terms of the EUPL-1.2
+icesdata=ICESStocks
 
-# LOAD raw dataset and polish
+rm(ICESStocks)
 
-# SAVE to ../data
+for (.id in subset(info,Model=="SMS")$.id)
+  icesdata[[.id]]=qapply(icesdata[[.id]], function(x) {
+    dimnames(x)[3:5]=dimnames(stock.n(icesdata[[.id]]))[3:5]
+    
+    dimnames(x)[[3]]="unique"
+    dimnames(x)[[4]]="all"
+    dimnames(x)[[5]]="unique"
+    x})
+
+for(.id in names(icesdata)){
+  small=min(catch.n(icesdata[[.id]]),na.rm=TRUE)*1e-6
+  catch.n(   icesdata[[.id]])[is.na(catch.n(   icesdata[[.id]]))]=small
+  landings.n(icesdata[[.id]])[is.na(landings.n(icesdata[[.id]]))]=small
+  discards.n(icesdata[[.id]])[is.na(catch.n(   icesdata[[.id]]))]=0.0}
+
+for (san in c("san.sa.1r","san.sa.2r","san.sa.3r","san.sa.4"))
+  icesdata[[san]]=qapply(icesdata[[san]], function(x) {
+    dimnames(x)$unit="unique"
+    x})
+
+save(icesdata,file="C:/active/flr/icesdata/data/icesdata.Rdata")
